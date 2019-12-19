c3c6ef8a67abeda1e3a20320d3f09f760bb10905
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2019-12-19 10:51:14 -0500

    Started repo, Added gitignore, made game layout, basic movement
    Began the repo for Pool with Math. Built very basic table and cue ball. Cue ball will speed up on mouse down and slow down on mouse release.

---
+++ BallRoll/Assets/movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float gravity = 1;
+    public float speed = 0;
+    public float force = 1;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            speed = speed + force;
+        }
+        else if (!Input.GetMouseButton(0) && speed > 0)
+        {
+            speed = speed - force;
+        }
+
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+    }
+}
