17cc70d3b53fe814f9f69991a0712b9d4f6dc97e
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2019-12-24 19:36:40 -0500

    Table built & Cueball speed/rotation
    Rebuilt the pool table, edited the formula for ball speed and rotation.

--- BallRoll/Assets/Scripts/movement.cs
+++ BallRoll/Assets/Scripts/movement.cs
@@ -6,6 +6,7 @@ public class movement : MonoBehaviour
 {
     public float gravity = 1;
     public float speed = 0;
+    public float friction = 2;
     public float force = 1;
 
     
@@ -22,11 +23,23 @@ public class movement : MonoBehaviour
         {
             speed = speed + force;
         }
-        else if (!Input.GetMouseButton(0) && speed > 0)
+        else if (!Input.GetMouseButton(0))
         {
-            speed = speed - force;
+            if (speed >= 0.5)
+            {
+                speed = speed - (friction * (Time.deltaTime * 10));
+            }
+            else if (speed <= -0.5)
+            {
+                speed = speed + (friction * (Time.deltaTime * 10));
+            }
+            else 
+            {
+                speed = 0;
+            }
         }
-
         transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        //this.transform.Rotate(speed, 0, 0);
+        print(speed);
     }
 }
