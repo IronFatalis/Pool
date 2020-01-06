c530463be0c330b8f2eed0394a1483630f2adac6
RiggsbyMortis <52928834+RiggsbyMortis@users.noreply.github.com>
2020-01-06 09:03:25 -0500

    moved initial ball code over to new manager

--- BallRoll/Assets/Scripts/Managers/BallManager.cs
+++ BallRoll/Assets/Scripts/Managers/BallManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour
 {
+    public float speed = 0;
+    public float force = 1;
+    public float friction = 2;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,32 @@ public class BallManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            speed = speed + force;
+        }
+        else if (!Input.GetMouseButton(0))
+        {
+            if (speed >= 0.5)
+            {
+                speed = speed - (friction * (Time.deltaTime * 10));
+            }
+            else if (speed <= -0.5)
+            {
+                speed = speed + (friction * (Time.deltaTime * 10));
+            }
+            else
+            {
+                speed = 0;
+            }
+        }
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
+        //this.transform.Rotate(speed, 0, 0);
+        print(speed);
     }
 }
--- BallRoll/Assets/Scripts/movement.cs
+++ BallRoll/Assets/Scripts/movement.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     public float gravity = 1;
-    public float speed = 0;
-    public float friction = 2;
-    public float force = 1;
-
+    
     
     // Start is called before the first frame update
     void Start()
@@ -19,27 +16,6 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButton(0))
-        {
-            speed = speed + force;
-        }
-        else if (!Input.GetMouseButton(0))
-        {
-            if (speed >= 0.5)
-            {
-                speed = speed - (friction * (Time.deltaTime * 10));
-            }
-            else if (speed <= -0.5)
-            {
-                speed = speed + (friction * (Time.deltaTime * 10));
-            }
-            else 
-            {
-                speed = 0;
-            }
-        }
-        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
-        //this.transform.Rotate(speed, 0, 0);
-        print(speed);
+
     }
 }
