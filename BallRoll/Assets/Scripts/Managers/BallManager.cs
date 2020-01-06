116ae67ee4d5fdcd6cae222b0e3966895e1420bd
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2020-01-06 10:37:46 -0500

    Cue ball bounce
    Cue ball can bounce off rails at an inverted angle.

--- BallRoll/Assets/Scripts/Managers/BallManager.cs
+++ BallRoll/Assets/Scripts/Managers/BallManager.cs
@@ -7,10 +7,11 @@ public class BallManager : MonoBehaviour
     public float friction = 2;
     public float mass = 2;
     public float acceleration = 20;
-    public float force = 0;
+    public float force = 1;
     public float velocity = 0;
     public float distance = 0;
     public float momentum = 0;
+    int inverse = -1;
     // Start is called before the first frame update
     void Start()
     {
@@ -51,6 +52,22 @@ public class BallManager : MonoBehaviour
         }
         transform.Translate(Vector3.forward * (velocity * Time.deltaTime));
         //this.transform.Rotate(speed, 0, 0);
+
+        if (this.transform.position.x >= 9 || this.transform.position.x <= -9)
+        {
+            //bounce without reflecting direction
+            Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y * inverse, transform.rotation.z, transform.rotation.w);
+            transform.rotation = rot;
+            //Vector3 Bump1 = new Vector3(9,transform.position.y,transform.position.z);
+        }
+        else if (this.transform.position.z >= 19 || this.transform.position.z <= -19)
+        {
+            //reflects direction and bounce
+            Quaternion rot = new Quaternion(this.transform.rotation.x, this.transform.rotation.y * inverse, this.transform.rotation.z, transform.rotation.w);
+            transform.rotation = rot;
+            velocity = velocity * inverse;
+        }
+
         print("Velocity");
         print(velocity);
     }
