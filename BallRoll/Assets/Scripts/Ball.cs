84f13c8fa42fdf4168764dde7127ba702faff902
RiggsbyMortis <52928834+RiggsbyMortis@users.noreply.github.com>
2020-01-07 10:44:52 -0500

    still playing w/ balls

--- BallRoll/Assets/Scripts/Ball.cs
+++ BallRoll/Assets/Scripts/Ball.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float b_fric = 2;
-    public float b_ma = 2;
-    private float b_accel = 0;
-    public float b_for = 1;
-    public float b_velo = 0;
-    public float b_dist = 0;
-    public float b_momen = 0;
+    public float b_fric = 2; //friction
+    public float b_ma = 2; //mass
+    private float b_accel = 0; //acceleration
+    public float b_for = 1; //force
+    public float b_velo = 0; //velocity
+    public float b_dist = 0; //unused
+    public float b_momen = 0; //momentum
     public float inverse = -1;
     int bumperSize = 1;
     int ballSize = 1;
@@ -25,12 +25,6 @@ public class Ball : MonoBehaviour
     Collider m_Collider1, m_Collider2;
     private Vector3 center;
     private Vector3 center2;
-
-
-    private Ball(float b_velocity)
-    {
-        this.b_velo = b_velocity;
-    }
 
     // Start is called before the first frame update
     void Start()
@@ -58,30 +52,34 @@ public class Ball : MonoBehaviour
     void Update()
     {
         B_Collide();
-        if (m_Collider1.bounds.Intersects(m_Collider2.bounds))
+        if (b_velo > 12)
         {
-            Debug.Log("hit!");
-            Debug.Log("extents: " + m_Collider1.bounds.extents);
-            Quaternion rot = new Quaternion(transform.rotation.x, 10, transform.rotation.z, transform.rotation.w);
-            transform.rotation = rot;
-            
+            b_velo = 12;
         }
     }
 
     void B_Collide()
     {
+        if (m_Collider1.bounds.Intersects(m_Collider2.bounds))
+        {
+            Debug.Log("hit!");
+            Debug.Log("extents: " + m_Collider1.bounds.extents);
+            b_velo = b_velo - (b_fric * (Time.deltaTime * 10));
+            Ball1.transform.Translate(Vector3.forward * (1));
+        }
         //colliding with rails
-        if (transform.position.x <= bumperLeft.transform.position.x + bumperSize || transform.position.x >= bumperRight.transform.position.x - bumperSize)
+        if (this.transform.position.x <= bumperLeft.transform.position.x + bumperSize || this.transform.position.x >= bumperRight.transform.position.x - bumperSize)
         {
             //bounce without reflecting direction
-            Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y * inverse, transform.rotation.z, transform.rotation.w);
+            Quaternion rot = new Quaternion(this.transform.rotation.x, this.transform.rotation.y * inverse, this.transform.rotation.z, this.transform.rotation.w);
             transform.rotation = rot;
         }
-        else if (transform.position.z >= bumperTop.transform.position.z - bumperSize || transform.position.z <= bumperBottom.transform.position.z + bumperSize)
+        else if (this.transform.position.z >= bumperTop.transform.position.z - bumperSize || this.transform.position.z <= bumperBottom.transform.position.z + bumperSize)
         {
             //reflects direction and bounce
-            Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y * inverse, transform.rotation.z, transform.rotation.w);
+            Quaternion rot = new Quaternion(this.transform.rotation.x, this.transform.rotation.y * inverse, this.transform.rotation.z, this.transform.rotation.w);
             transform.rotation = rot;
+            b_velo *= inverse;
         }
     }
 }
