c7b42e36245c9d406dd3d4d6ca096fa381a0a48c
RiggsbyMortis <52928834+RiggsbyMortis@users.noreply.github.com>
2020-01-07 10:51:46 -0500

    still playing w/ balls again

--- BallRoll/Assets/Scripts/Managers/TableManager.cs
+++ BallRoll/Assets/Scripts/Managers/TableManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TableManager : MonoBehaviour
 {
+    public float gravity = 10;
+    public float n_force = 10;
+    private GameObject bumperLeft;
+    private GameObject bumperRight;
+    private GameObject bumperTop;
+    private GameObject bumperBottom;
+    private GameObject table;
+    private GameObject CueBall, Ball1;
+
     // Start is called before the first frame update
     void Start()
     {
