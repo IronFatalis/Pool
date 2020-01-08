5d1e889f9aa820317f4f2544b6751db7788e8a20
RiggsbyMortis <52928834+RiggsbyMortis@users.noreply.github.com>
2020-01-08 11:27:10 -0500

    actually made some progress

--- BallRoll/Assets/Scripts/Ball.cs
+++ BallRoll/Assets/Scripts/Ball.cs
@@ -4,19 +4,20 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    public float b_fric = 2; //friction
-    public float b_ma = 2; //mass
-    private float b_accel = 0; //acceleration
-    public float b_for = 1; //force
-    public float b_velo = 0; //velocity
-    public float b_dist = 0; //unused
-    public float b_momen = 0; //momentum
+    public float friction = 2; //friction
+    public float mass = 2; //mass
+    public float acceleration = 0; //acceleration
+    public float force = 1; //force
+    public float velocity = 0; //velocity
+    public float distance = 0; //unused
+    public float momentum = 0; //momentum
     public float inverse = -1;
     int bumperSize = 1;
     int ballSize = 1;
     public float b_radius = 0.5f;
     Bounds b;
     bool hit = false;
+    public Transform target;
 
     private GameObject bumperLeft;
     private GameObject bumperRight;
@@ -26,13 +27,15 @@ public class Ball : MonoBehaviour
     Collider m_Collider1, m_Collider2;
     private Vector3 center;
     private Vector3 center2;
+    private Vector3 targetDir;
+    private Vector3 forward;
 
     // Start is called before the first frame update
     void Start()
     {
-        b_accel = b_ma * b_for;
-        b_velo = b_accel / Time.deltaTime;
-        b_momen = b_ma * b_velo;
+        acceleration = mass * force;
+        velocity = acceleration / Time.deltaTime;
+        momentum = mass * velocity;
 
         b = new Bounds(new Vector3(0, 0, 0), new Vector3(2, 2, 2));
 
@@ -50,28 +53,28 @@ public class Ball : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    public virtual void Update()
     {
         B_Collide();
-        if (b_velo > 12)
+        if (velocity > 12)
         {
-            b_velo = 12;
+            velocity = 12;
         }
         if (hit == true)
         {
-            Ball1.transform.Translate(Vector3.forward * (b_velo * Time.deltaTime));
+            Ball1.transform.Translate(Vector3.forward * (velocity * Time.deltaTime));
         }
     }
 
     void B_Collide()
     {
-        if (m_Collider1.bounds.Intersects(m_Collider2.bounds))
+        /*if (m_Collider1.bounds.Intersects(m_Collider2.bounds))
         {
-            Debug.Log("hit!");
-            Debug.Log("extents: " + m_Collider1.bounds.extents);
-            b_velo = b_velo - (b_fric * (Time.deltaTime * 10));
+            print("hit!");
+            velocity = velocity - (friction * (Time.deltaTime * 10));
             hit = true;
-        }
+        }*/
+
         //colliding with rails
         if (this.transform.position.x <= bumperLeft.transform.position.x + bumperSize || this.transform.position.x >= bumperRight.transform.position.x - bumperSize)
         {
@@ -84,7 +87,26 @@ public class Ball : MonoBehaviour
             //reflects direction and bounce
             Quaternion rot = new Quaternion(this.transform.rotation.x, this.transform.rotation.y * inverse, this.transform.rotation.z, this.transform.rotation.w);
             transform.rotation = rot;
-            b_velo *= inverse;
+            velocity *= inverse;
+        }
+    }
+
+    void angleChk()
+    {
+        Vector3 targetDir = target.position - transform.position;
+        Vector3 forward = transform.forward;
+        float angle = Vector3.SignedAngle(targetDir, forward, Vector3.up);
+        if (angle < -5.0f)
+        {
+            print("turn left");
+        }
+        else if (angle > 5.0f)
+        {
+            print("turn right");
+        }
+        else
+        {
+            print("forward");
         }
     }
 }
--- BallRoll/Assets/Scripts/Managers/BallManager.cs
+++ BallRoll/Assets/Scripts/Managers/BallManager.cs
@@ -4,114 +4,35 @@ using UnityEngine;
 
 public class BallManager : MonoBehaviour
 {
-    public float friction = 2;
-    public float mass = 2;
-    public float acceleration = 20;
-    public float force = 1;
-    public float velocity = 0;
-    public float distance = 0;
-    public float momentum = 0;
-    float ballSize = 1;
-
-    bool cueMovement = false;
-    int inverse = -1;
-    int bumperSize = 1;
-    private GameObject bumperLeft;
-    private GameObject bumperRight;
-    private GameObject bumperTop;
-    private GameObject bumperBottom;
-    private GameObject CueBall;
-    private GameObject Ball1;
+    public List<Ball> BallList;
+    public Vector3 impact;
 
     // Start is called before the first frame update
     void Start()
     {
-        bumperLeft = GameObject.Find("BumperLeft");
-        bumperRight = GameObject.Find("BumperRight");
-        bumperTop = GameObject.Find("BumperTop");
-        bumperBottom = GameObject.Find("BumperBottom");
-        CueBall = GameObject.Find("CueBall");
-        Ball1 = GameObject.Find("Balls");
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (velocity != 0)
+        foreach(Ball ballFirst in BallList)
         {
-            cueMovement = true;
-        }
-        else
-        {
-            cueMovement = false;
-        }
-        Movement();
-    }
-
-    void Movement()
-    {
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            //Create a ray going into the scene from the screen location
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            //the raycast hit info which will be filled by the phsics.Raycast()
-            RaycastHit hit;
-
-
-            //Perform a raycast using our new ray
-            if (Physics.Raycast(ray, out hit))
+            foreach(Ball ballSecond in BallList)
             {
-                //if a collision occured. check if its our ball
-                if (hit.collider.gameObject.tag == "CueBall" && cueMovement == false)
+                if(ballFirst.name != ballSecond.name)
                 {
-                    acceleration = mass * force;
-                    velocity = acceleration / Time.deltaTime;
-                    momentum = mass * velocity;
-                    print(hit.point);
+                    impact = ballSecond.transform.position - ballFirst.transform.position;
+                    float ballDistance = Mathf.Sqrt(Mathf.Pow(impact.x, 2) + Mathf.Pow(impact.y, 2) + Mathf.Pow(impact.z, 2));
+                    if (ballDistance < 1)
+                    {
+                        print(ballDistance);
+
+                    }
+                    
                 }
             }
         }
-        if (cueMovement == true)
-        {
-            if (velocity >= 0.5)
-            {
-                velocity = velocity - (friction * (Time.deltaTime * 10));
-            }
-            else if (velocity <= -0.5)
-            {
-                velocity = velocity + (friction * (Time.deltaTime * 10));
-            }
-            else
-            {
-                velocity = 0;
-            }
-        }
-
-        transform.Translate(Vector3.forward * (velocity * Time.deltaTime));
-        //this.transform.Rotate(speed, 0, 0);
-
-        if (this.transform.position.x <= bumperLeft.transform.position.x + bumperSize || this.transform.position.x >= bumperRight.transform.position.x - bumperSize)
-        {
-            //bounce without reflecting direction
-            Quaternion rot = new Quaternion(transform.rotation.x, transform.rotation.y * inverse, transform.rotation.z, transform.rotation.w);
-            transform.rotation = rot;
-        }
-        else if (this.transform.position.z >= bumperTop.transform.position.z - bumperSize || this.transform.position.z <= bumperBottom.transform.position.z + bumperSize)
-        {
-            //reflects direction and bounce
-            Quaternion rot = new Quaternion(this.transform.rotation.x, this.transform.rotation.y * inverse, this.transform.rotation.z, transform.rotation.w);
-            transform.rotation = rot;
-            velocity = velocity * inverse;
-        }
-
-        //print("Velocity");
-        //print(velocity);
-
     }
-
-    //sphere on sphere collision
 
 }
